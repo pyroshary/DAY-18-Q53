ada585fcc818dfbd67e2805e5a797550b227c483
pyroshary <sheharyarghulamrasool@gmail.com>
2024-03-21 02:32:12 +0500

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//crating programmer skills list
+var developerskills = {
+    language: ["HTML", "CSS", "JAVASCRIPT"],
+    frameworks: ["React", "Angular", "vue"],
+    tools: ["git", "web", "dock"]
+};
+//gettting speicific names
+var language = developerskills.language, frameworks = developerskills.frameworks, tools = developerskills.tools;
+console.log("language: ".concat(language[2], ",frameworks: ").concat(frameworks[2], ",tools: ").concat(tools[2]));
---
+++ index.ts
@@ -0,0 +1,10 @@
+ //crating programmer skills list
+ let developerskills = {
+    language:["HTML","CSS","JAVASCRIPT"],
+    frameworks:["React","Angular","vue"],
+    tools:["git","web","dock"]
+ }
+ //gettting speicific names
+ let{language,frameworks,tools} =developerskills;
+
+console.log(`language: ${language[2]},frameworks: ${frameworks[2]},tools: ${tools[2]}`);
